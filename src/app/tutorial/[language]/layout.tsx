16dce22f8928fc17ed6b4f2d7a072f84c72cad0e
Akhand Pratap singh <Aksingh6886@gmail.com>
2025-08-07 10:22:09 +0000

    all of them are not added checl again

--- src/app/tutorial/[language]/layout.tsx
+++ src/app/tutorial/[language]/layout.tsx
@@ -1,6 +1,7 @@
 // src/app/tutorial/[language]/layout.tsx
 import { allTutorialPosts } from 'contentlayer/generated';
 import { TutorialSidebar } from '@/components/layout/tutorial-sidebar';
+import { LanguageScroller } from '@/components/layout/language-scroller';
 
 export default function TutorialLayout({
   children,
@@ -24,17 +25,20 @@ export default function TutorialLayout({
 
 
   return (
-    <div className="container mx-auto">
-        <div className="flex flex-col md:flex-row gap-8">
-            <aside className="w-full md:w-64 lg:w-72 flex-shrink-0 md:py-6 md:pl-2">
-                <div className="sticky top-16 h-[calc(100vh-4rem)] overflow-y-auto">
-                    <TutorialSidebar groupedTutorials={groupedTutorials} />
-                </div>
-            </aside>
-            <main className="flex-grow min-w-0 md:pr-8">
-                {children}
-            </main>
-        </div>
+    <div className="container px-4 sm:px-6">
+      <div className="py-6">
+        <LanguageScroller activeLanguage={params.language} />
+      </div>
+      <div className="flex flex-col md:flex-row gap-8">
+        <aside className="w-full md:w-64 lg:w-72 flex-shrink-0 md:py-6 md:pl-2">
+            <div className="sticky top-16 h-[calc(100vh-4rem)] overflow-y-auto">
+                <TutorialSidebar groupedTutorials={groupedTutorials} />
+            </div>
+        </aside>
+        <main className="flex-grow min-w-0 md:pr-8">
+          {children}
+        </main>
+      </div>
     </div>
   );
 }
