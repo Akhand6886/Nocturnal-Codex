690ba73499cb63a966e5d542c6b64cfa75ee21bf
Akhand Pratap singh <Aksingh6886@gmail.com>
2025-08-07 09:50:06 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="sticky

--- src/app/tutorial/[language]/layout.tsx
+++ src/app/tutorial/[language]/layout.tsx
@@ -24,15 +24,17 @@ export default function TutorialLayout({
 
 
   return (
-    <div className="flex flex-col md:flex-row">
-      <aside className="w-full md:w-64 lg:w-72 flex-shrink-0 md:p-6">
-        <div className="sticky top-16 h-[calc(100vh-4rem)] overflow-y-auto">
-          <TutorialSidebar groupedTutorials={groupedTutorials} />
+    <div className="container mx-auto max-w-7xl">
+        <div className="flex flex-col md:flex-row gap-8">
+            <aside className="w-full md:w-64 lg:w-72 flex-shrink-0 md:py-6">
+                <div className="sticky top-16 h-[calc(100vh-4rem)] overflow-y-auto">
+                    <TutorialSidebar groupedTutorials={groupedTutorials} />
+                </div>
+            </aside>
+            <main className="flex-grow min-w-0 pt-8 md:pr-8">
+                {children}
+            </main>
         </div>
-      </aside>
-      <main className="flex-grow min-w-0 md:border-l md:pl-8 pt-8 md:pr-8">
-        {children}
-      </main>
     </div>
   );
 }
