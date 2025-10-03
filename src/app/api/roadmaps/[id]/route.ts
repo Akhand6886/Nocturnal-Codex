b26d31950d97980244b79ea6d640fd7a65e9c8cc
Akhand Pratap singh <Aksingh6886@gmail.com>
2025-10-03 16:59:52 +0000

    fixed issue

--- src/app/api/roadmaps/[id]/route.ts
+++ src/app/api/roadmaps/[id]/route.ts
@@ -1,11 +1,9 @@
-
-import type { NextRequest } from 'next/server';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(
   request: NextRequest,
   context: { params: { id: string } }
 ) {
-  const id = context.params.id;
-  return NextResponse.json({ message: `API route for roadmap: ${id}` });
+  const { id } = context.params
+  return NextResponse.json({ message: `API route for roadmap: ${id}` })
 }
