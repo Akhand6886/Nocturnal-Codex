cb6a0e40f11d9da3f9f03a316ba83cd9f29563ad
Akhand Pratap Singh <aksingh6886@gmail.com>
2026-05-04 23:31:58 +0530

    fix(ui): resolve SiCsharp export error by using SiDotnet

--- src/components/common/simple-icon.tsx
+++ src/components/common/simple-icon.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/lib/utils";
 import { Terminal } from "lucide-react";
 import {
-  SiC, SiCplusplus, SiCsharp, SiCss3, SiDart, SiElixir, SiGo, SiHaskell, SiHtml5, SiJava, SiJavascript, SiKotlin, SiPython, SiR, SiRuby, SiRust, SiScala, SiSwift, SiTypescript, SiZig, SiPhp, SiPerl, SiSolidity, SiGnubash, SiMysql, SiApple
+  SiC, SiCplusplus, SiCss3, SiDart, SiElixir, SiGo, SiHaskell, SiHtml5, SiJava, SiJavascript, SiKotlin, SiPython, SiR, SiRuby, SiRust, SiScala, SiSwift, SiTypescript, SiZig, SiPhp, SiPerl, SiSolidity, SiGnubash, SiMysql, SiApple, SiDotnet
 } from 'react-icons/si';
 
 interface SimpleIconProps extends React.SVGProps<SVGSVGElement> {
@@ -12,8 +12,8 @@ const iconMap: Record<string, React.ElementType> = {
   "c": SiC,
   "c++": SiCplusplus,
   "cplusplus": SiCplusplus,
-  "c#": SiCsharp,
-  "csharp": SiCsharp,
+  "c#": SiDotnet,
+  "csharp": SiDotnet,
   "css": SiCss3,
   "dart": SiDart,
   "elixir": SiElixir,
